9a2be9732a9fea348621d992ff51366fd3f08692
bonanno_fernando@hotmail.com <bonanno_fernando@hotmail.com>
2025-08-26 12:56:51 -0300

    commit initial

---
+++ src/main/java/com/example/ProyectoSantander/ProyectoSantanderApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoSantander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSantanderApplication.class, args);
+	}
+
+}
